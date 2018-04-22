c76d5f5f8bb06724ef6703f04a10a0e15474e064
Christian Kokott <christian.kokott@googlemail.com>
2018-04-22 17:18:42 +0200

    skeleton

--- index.js
+++ index.js
@@ -2,21 +2,20 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
 
-app.use('/js', express.static('js'));
-app.use('/ext_js',express.static('ext_js'));
-app.use('/models',express.static('models'));
-app.use('/css',express.static('css'));
-app.use('/images',express.static('images'));
+app.use('/static', express.static('public/static'));
+app.use('/js', express.static('public/js'));
+app.use('/css', express.static('public/css'));
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-  });
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
   
-  http.listen(3000, function(){
-    console.log('listening on *:3000');
-  });
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
   
-  io.on('connection', function(socket){
-    console.log('a user connected');
-  });
+io.on('connection', function(socket) {
+  console.log('a user connected');
+});
