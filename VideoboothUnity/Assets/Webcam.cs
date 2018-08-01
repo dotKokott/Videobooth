28ac122124cc7adad42c82bc87cc744226ac127a
Christian Kokott <christian.kokott@googlemail.com>
2018-08-01 14:18:05 +0200

    mikrophone and shit working

--- VideoboothUnity/Assets/Webcam.cs
+++ VideoboothUnity/Assets/Webcam.cs
@@ -5,8 +5,33 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR.WSA.WebCam;
+using System.Runtime.InteropServices;
+using System;
 
 public class Webcam : MonoBehaviour {
+
+    private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+    private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+    private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+    private const int WM_APPCOMMAND = 0x319;
+ 
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+    public void MuteWindows() {
+        var handle = GetWindowHandle();
+        
+        SendMessageW(handle, WM_APPCOMMAND, handle, (IntPtr)APPCOMMAND_VOLUME_MUTE);
+    }
+
+    [DllImport("user32.dll")]
+    private static extern System.IntPtr GetActiveWindow();
+ 
+    public static System.IntPtr GetWindowHandle() {
+        return GetActiveWindow();
+    }
+
 
 	Renderer ren;
 
@@ -30,6 +55,14 @@ public class Webcam : MonoBehaviour {
 
 	private AudioSource microphone;
 
+    public float RecordingTime = 30f;    
+
+    private DateTime currentTime;
+    private float currentTimeFloat;
+
+    public Text TimeText;
+    public GameObject Panel;
+
 	void Start () {
         var quadHeight = Camera.main.orthographicSize * 2f;
         var quadWidth = quadHeight * Screen.width / Screen.height;
@@ -38,19 +71,22 @@ public class Webcam : MonoBehaviour {
         webcamTexture = new WebCamTexture(1600, 869, 30);
         webcamTexture.hideFlags = HideFlags.HideAndDontSave;
 
+        MuteWindows();
+
+        currentTime = new DateTime(2018, 1, 1, 0, 0, 0, 0);
+        currentTime = currentTime.AddSeconds(RecordingTime);
+        currentTimeFloat = RecordingTime;
+        TimeText.text = currentTime.ToString("mm:ss.ff");
+
 		microphone = GetComponent<AudioSource> ();
-		microphone.clip = Microphone.Start ("Mikrofon", true, 10, 44100);
+		microphone.clip = Microphone.Start (null, true, 10, 44100);
 		microphone.loop = true;
 
-		while (!(Microphone.GetPosition(null) > 0)){}
+		//while (!(Microphone.GetPosition(null) > 0)){}
 
 		microphone.Play ();
         ren = GetComponent<Renderer>();        
-        StartCam();
-
-        
-        if(Recording) UpdateColor(0);
-        if(Pause) Pause.color = Clear;
+        StartCam();      
 
         PathConfig.SaveFolder = SavePath;
 	}
@@ -87,11 +123,27 @@ public class Webcam : MonoBehaviour {
         if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetAxis("Mouse ScrollWheel") < 0f) PreviousSide();
 
         if(CaptureControl.status == VideoCaptureCtrl.StatusType.FINISH && !Replaying) {
+            Microphone.End(null);
+            TimeText.text = "";
+            
+            MuteWindows(); //Unmute
+
             Replaying = true;                        
 
             VideoPlayer.instance.enabled = true;
             VideoPlayer.instance.SetRootFolder();
             VideoPlayer.instance.PlayLastVideo();            
+        }
+
+        if(CaptureControl.status == VideoCaptureCtrlBase.StatusType.STARTED) {
+            currentTime = currentTime.AddSeconds(Time.deltaTime * -1);
+            currentTimeFloat -= Time.deltaTime;
+            TimeText.text = currentTime.ToString("mm:ss.ff");
+
+            if(currentTimeFloat <= 0) {
+                CaptureControl.StopCapture(); 	
+                microphone.enabled = false;          
+            }
         }
 
         if(Input.GetKeyDown(KeyCode.Space)) {
@@ -105,6 +157,7 @@ public class Webcam : MonoBehaviour {
 
                 CaptureControl.RemoveInstance();
                 VideoPlayer.instance = null;
+
                 SceneManager.LoadScene(0);
             } else {
                 Record();
@@ -114,21 +167,11 @@ public class Webcam : MonoBehaviour {
 
     public void Record() {
         if(CaptureControl.status == VideoCaptureCtrlBase.StatusType.STARTED) {
-            CaptureControl.StopCapture(); 
-			Microphone.End ("Mikrofon");
-            iTween.Stop();
-            Recording.color = Clear;
-        } else {
+            CaptureControl.StopCapture(); 	
+            microphone.enabled = false;
+            TimeText.text = "WAIT";
+        } else if(CaptureControl.status == VideoCaptureCtrlBase.StatusType.NOT_START) {
             CaptureControl.StartCapture();
-            float flashSpeed = 0.5f;
-            iTween.ValueTo(gameObject, iTween.Hash("from", 0f, "to", 1f, "time", flashSpeed, "easeType", iTween.EaseType.linear, "loopType", iTween.LoopType.pingPong, "onUpdate", "UpdateColor"));
-        }
-
-
-        
-    }
-
-    public void UpdateColor(float value) {
-        Recording.color = new Color(White.r, White.g, White.b, value);
+        }        
     }
 }
